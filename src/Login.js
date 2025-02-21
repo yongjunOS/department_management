4f663ee8cf01137c0b7779ed6ebca44a768d6be0
김용준 <fnsl223@livon.care>
2025-02-21 17:47:13 +0900

    [fix] 로그인 div 수정정

--- src/Login.js
+++ src/Login.js
@@ -11,13 +11,11 @@ class Login extends React.Component {
                     <div>
                         <h1 id='login_title'>로그인</h1>
                     </div>
-                   
                         <div className="input">
                             <input type="text" className="userId" id="userId" placeholder="아이디" autoFocus></input>
                             <input type="password" className="password" id="password" placeholder="비밀번호"></input>
                             <button id="loginBut">Login</button>
                         </div>
-                       
                 </form>
             </div>
         )
